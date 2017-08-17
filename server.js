4f570a35b99a6f24af8b548fd0a17173584fb05e
manojagrawal226 <30975389+manojagrawal226@users.noreply.github.com>
2017-08-17 20:17:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName',function (req, res){
     //articleNme = article-one
     //articleName[articles]= {} contents of articles
-    var articleName = req.param.articleName;
+    var articleName = req.params.articleName;
      res.send(createTemplate(articles[articleName]));
 });
 
