60c59fee49de5aa5d1841974e58ad3a13f951b63
manojagrawal226 <30975389+manojagrawal226@users.noreply.github.com>
2017-08-18 19:27:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ element.innerHTML="New value";
 // move image to left
 
 var img = document.getElementById('modi');
+var marginLeft =0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + "px";
+}
+    
 img.onclick = function() {
-    img.style.marginLfet = '100px';
+    var interval = setInterval(moveRight,100);
 };
