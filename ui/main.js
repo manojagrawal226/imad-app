d0b4d558faa42a5a2552b1f644fce9565408b2f4
manojagrawal226 <30975389+manojagrawal226@users.noreply.github.com>
2017-08-18 19:12:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
 var element = document.getElementById('main-txt');
 element.InnerHTML="New value";
+
+// move image to left
+
+var img = document.getElementById('modi');
+img.onclick = function() {
+    img.style.marginefet = '100px';
+};
