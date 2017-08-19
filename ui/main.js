a6dbbb09c2c49f6b27d4ad01049b08776458c8e4
manojagrawal226 <30975389+manojagrawal226@users.noreply.github.com>
2017-08-19 18:30:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,9 +6,32 @@ element.innerHTML="New value";
 
 var img = document.getElementById('modi');
 var marginLeft =0;
+var marginUp = 0;
+var marginRight =0;
+var marginDown = 0;
 function moveRight(){
+    if (marginLeft>=0 && marginLeft < 61){
+        marginRight = 0;
+        marginUp = 0;
+        marginDown = 0;
     marginLeft = marginLeft + 5;
-    img.style.marginLeft = marginLeft + "px";
+    img.style.marginLeft = marginLeft + "px";}
+    if (marginLeft > 60){
+        marginUp = marginUP + 5;
+        img.style.marginUp = marginUp + "px";
+    }
+    if (marginUp > 60){
+        marginLeft = 10;
+        marginRight = marginRight + 5;
+        img.style.marginRight=marginRight + "px";
+    }
+    if (marginRight > 60){
+        marginUp = 0;
+        marginDown = marginDown + 5;
+        img.style.marginDown = marginDown + "px";
+    }
+        
+        
 }
     
 img.onclick = function() {
